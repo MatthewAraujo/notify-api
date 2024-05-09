037ef352808f0962e53e091e48780f74d75e8b01
MatthewAraujo <matthewaraujo20@gmail.com>
2024-05-09 15:22:34 -0300

    feat: choose notify

--- cmd/types/types.go
+++ cmd/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "github.com/google/uuid"
+
 type GithubWebhooks struct {
 	Repository struct {
 		FullName string `json:"full_name"`
@@ -23,4 +25,70 @@ type SendEmail struct {
 	Sender   string
 	Commit   string
 	Email    string
+}
+
+type NotificationStore interface {
+	GetUserByID(id uuid.UUID) (Notifications, error)
+	GetRepositoryByUserID(id uuid.UUID, reponame string) (Notifications, error)
+	CreateNotification(notif Notifications) error
+}
+type Notifications struct {
+	UserId   uuid.UUID `json:"user_id"`
+	RepoName string    `json:"repository_name"`
+	Events   Events    `json:"events"`
+}
+
+type Events struct {
+	BranchCreation                bool
+	TagCreation                   bool
+	BranchDeletion                bool
+	TagDeletion                   bool
+	BranchProtectionConfig        bool
+	BranchProtectionRules         bool
+	CheckRuns                     bool
+	CheckSuites                   bool
+	CodeScanningAlerts            bool
+	CollaboratorChange            bool
+	CommitComments                bool
+	DependabotAlerts              bool
+	DeployKeys                    bool
+	DeploymentStatuses            bool
+	Deployments                   bool
+	DiscussionComments            bool
+	Discussions                   bool
+	Forks                         bool
+	IssueComments                 bool
+	Issues                        bool
+	Labels                        bool
+	MergeGroups                   bool
+	Meta                          bool
+	Milestones                    bool
+	Packages                      bool
+	PageBuilds                    bool
+	ProjectCards                  bool
+	ProjectColumns                bool
+	Projects                      bool
+	PullRequestReviewComments     bool
+	PullRequestReviewThreads      bool
+	PullRequestReviews            bool
+	PullRequests                  bool
+	Pushes                        bool
+	RegistryPackages              bool
+	Releases                      bool
+	Repositories                  bool
+	RepositoryAdvisories          bool
+	RepositoryImports             bool
+	RepositoryRulesets            bool
+	RepositoryVulnerabilityAlerts bool
+	SecretScanningAlertLocations  bool
+	SecretScanningAlerts          bool
+	SecurityAndAnalyses           bool
+	Stars                         bool
+	Statuses                      bool
+	TeamAdds                      bool
+	VisibilityChanges             bool
+	Watches                       bool
+	Wiki                          bool
+	WorkflowJobs                  bool
+	WorkflowRuns                  bool
 }
