5df3480492b9f294617c0fcaf7885f0cd7f20997
MatthewAraujo <matthewaraujo20@gmail.com>
2024-05-17 22:33:29 -0300

    feat: never expires

--- auth/jwt.go
+++ auth/jwt.go
@@ -4,7 +4,6 @@ import (
 	"crypto/x509"
 	"database/sql"
 	"encoding/pem"
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -19,26 +18,13 @@ import (
 
 func GenerateJWT() (string, error) {
 	token, err := getJwt()
-	tokenIsExpired := false
 	if err != nil {
 		if err.Error() == "token not found" {
 			log.Printf("Token not found, generating new token")
 		}
 	} else {
-
-		tokenIsExpired, err = isJwtExpired(token)
-		if err != nil {
-			return "", err
-		}
-
-		if !tokenIsExpired {
-			log.Printf("Token is not expired")
-			return token, nil
-		}
-
-		log.Printf("Token is expired, generating new token")
+		return token, nil
 	}
-
 	godotenv.Load()
 
 	app_id := os.Getenv("APP_ID")
@@ -73,17 +59,7 @@ func GenerateJWT() (string, error) {
 		return "", err
 	}
 
-	if tokenIsExpired {
-		err = UpdateJwtToken(tokenString)
-		if err != nil {
-			return "", err
-		}
-	} else {
-		err = InsertJwtToken(tokenString)
-		if err != nil {
-			return "", err
-		}
-	}
+	err = InsertJwtToken(tokenString)
 
 	return tokenString, nil
 }
@@ -95,20 +71,6 @@ func InsertJwtToken(token string) error {
 	}
 
 	_, err = db.Exec("INSERT INTO JwtToken (token) VALUES (?)", token)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func UpdateJwtToken(token string) error {
-	db, err := db.NewMySQLStorage(config.Envs.TursoURl)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec("UPDATE JwtToken SET token = ?", token)
 	if err != nil {
 		return err
 	}
@@ -139,44 +101,4 @@ func getJwt() (string, error) {
 	log.Printf("Token found in database")
 
 	return token, nil
-}
-
-func isJwtExpired(tokenString string) (bool, error) {
-	keyPath := "key.pem"
-	// Carrega a chave pública do arquivo PEM
-	keyData, err := utils.ReadFile(keyPath)
-	if err != nil {
-		return false, fmt.Errorf("erro ao ler o arquivo de chave: %v", err)
-	}
-
-	publicKey, err := jwt.ParseRSAPublicKeyFromPEM(keyData)
-	if err != nil {
-		return false, fmt.Errorf("erro ao analisar a chave pública: %v", err)
-	}
-
-	// Análise e verificação do token JWT
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Verifique se o método de assinatura é RSA
-		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("método de assinatura inesperado: %v", token.Header["alg"])
-		}
-		return publicKey, nil
-	})
-
-	if err != nil {
-		return false, fmt.Errorf("erro ao analisar o token JWT: %v", err)
-	}
-
-	// Verifica se o token é válido
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		// Verifica a expiração
-		if exp, ok := claims["exp"].(float64); ok {
-			expirationTime := time.Unix(int64(exp), 0)
-			return expirationTime.Before(time.Now()), nil
-		} else {
-			return false, errors.New("o campo 'exp' não está presente no token")
-		}
-	} else {
-		return false, errors.New("token JWT inválido")
-	}
 }
