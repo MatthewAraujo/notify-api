c655e5a6586c419ff4e04fc1afe45c24183bbe6d
MatthewAraujo <matthewaraujo20@gmail.com>
2024-06-24 20:08:05 -0300

    cors

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -30,12 +30,12 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 func (s *APIServer) Start() error {
 	router := mux.NewRouter()
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowOriginFunc:  func(origin string) bool { return true },
+		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"*"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
-		Debug:            true,
+		MaxAge:           300,
 	})
 
 	router.Use(c.Handler)
